36234f2f9b28cf80ff617848ef6b4ac2d709edb4
Lan-sx <shexin@ihep.ac.cn>
2024-09-07 20:52:59 +0800

    for (chip,channel) <-> (row,col) and define some global params

---
+++ include/BeamUnities.h
@@ -0,0 +1,80 @@
+/*********************************************************************
+ * Author           : Lan-sx
+ * Email            : shexin@ihep.ac.cn
+ * Last modified    : 2024-09-04 19:51
+ * Filename         : BeamUnities.h
+ * Description      : 
+ * Update           : 
+ * ******************************************************************/
+#ifndef __BeamUnities_H__
+#define __BeamUnities_H__ 1
+
+//std 
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <sstream>
+#include <map>
+#include <algorithm>
+#include <cmath>
+
+//global pars
+constexpr int  __ROW__=10;
+constexpr int  __COL__=300;
+constexpr int  __NumChip__=24;
+constexpr int __NumChn__=128;
+
+constexpr double PixelSize = 0.04; // [cm]
+constexpr double PixelPitch = 0.01; // [cm]
+constexpr double PixelSpace = PixelSize+PixelPitch;
+constexpr double Ylowboundary = -PixelSize/2;  // [cm]
+constexpr double Yupboundary = (PixelSize+PixelPitch)*__COL__+Ylowboundary;  // [cm]
+constexpr double Xlowboundary = -PixelSize/2.;
+constexpr double X_upboundary = (PixelSize+PixelPitch)*__ROW__+Xlowboundary;
+
+//Diffiusion pars, from Yue Chang 
+constexpr double X_Cd = 8.66021e-3;  // [sqrt(cm)]
+constexpr double Y_Cd = 8.71839e-3;  // [sqrt(cm)]
+constexpr double Sigma_aval = 0.003; // [cm]
+constexpr double Velo_e = 8.; // [cm/us] from Magboltz
+
+//Simulation Pars for MC data
+constexpr double MaxDriftLength = 50.; // [cm]
+constexpr double MaxLengthY = 20.; // [cm]
+
+namespace BeamUnities
+{
+// Maps Chip,Channel index <-> Row,Col index
+// @param std::string ChipChnmapfile, config csv file 
+std::vector<std::pair<int,int>> CreateChipChnToRowColMap(std::string ChipChnmapfile);
+
+// Row, Col index -> Chip, Channel index
+// @param int row, row index, from 0 to __ROW__ -1
+// @param int col, col index, from 0 to __COL__-1
+// @parma vMaps, maps
+std::pair<int,int> RowColIdx2ChipChn(int row, int col, const std::vector<std::pair<int,int>> vMaps);
+std::pair<int,int> RowColIdx2ChipChn(std::pair<int,int> rowcolpair,const std::vector<std::pair<int,int>> vMaps);
+
+// Chip, Channel index -> Row, Col index
+// @param int chipIdx, chip index, from 0 to __NumChip__ -1
+// @param int chnIdx, channel index, from 0 to __NumChn__ -1
+std::pair<int,int> ChipChn2RowCol(int chipIdx, int chnIdx,const std::vector<std::pair<int,int>> vMaps);
+std::pair<int,int> ChipChn2RowCol(std::pair<int,int> chipchnpair,const std::vector<std::pair<int,int>> vMaps);
+
+// Position (x,y) -> pixel row col index
+// @param double px, position x
+// @param double py, position y
+std::pair<int,int> Position2RowCol(double px, double py);
+std::pair<int,int> Position2RowCol(std::pair<double,double> positionpair);
+
+// Pixel row col index -> Pixel Center Position (x,y)
+// @param int row, row index
+// @param int col, col index
+std::pair<double,double> RowColIdx2Position(int row, int col);
+std::pair<double,double> RowColIdx2Position(std::pair<int,int> rowcolpair);
+
+}
+
+
+
+#endif
---
+++ src/BeamUnities.cpp
@@ -0,0 +1,141 @@
+/*********************************************************************
+ * Author           : Lan-sx
+ * Email            : shexin@ihep.ac.cn
+ * Last modified    : 2024-09-04 20:01
+ * Filename         : BeamUnities.cpp
+ * Description      : 
+ * Update           : 
+ * ******************************************************************/
+#include "BeamUnities.h"
+
+std::vector<std::pair<int,int>> BeamUnities::CreateChipChnToRowColMap(std::string ChipChnmapfile)
+{
+    std::vector<std::pair<int,int>> vMaps;
+
+    std::ifstream fin(ChipChnmapfile.data(),std::ios::in);
+    if(!fin.is_open())
+    {
+        std::printf("########!!!!Error!!! Maps file doesn't exist\n");
+
+    }
+    std::string lines;
+    //vMaps.resize(__ROW__*__COL__);
+
+    //skip header line
+    std::getline(fin,lines);
+    
+    while(std::getline(fin,lines))
+    {
+        std::istringstream ss(lines);
+        std::string token;
+        std::vector<int> rows;
+        
+        int tmpcnts=0;
+        while(std::getline(ss,token,','))
+        {
+            rows.push_back(std::stoi(token));
+        }
+        
+        vMaps.push_back(std::make_pair(rows.at(1),rows.at(2)));
+    }
+    
+    fin.close();
+
+    return vMaps;
+}
+
+std::pair<int,int> BeamUnities::RowColIdx2ChipChn(int row, int col,const std::vector<std::pair<int,int>> vMaps)
+{
+    if(row<0 || row>=__ROW__ || col<0 || col>=__COL__) 
+    {
+        std::printf(">>>>>>>>>>>>>>>>>>>>>> Error!!! row col in RowColIdx2ChipChn\n");
+        return std::make_pair(-1,-1);
+    }
+    
+    int IdxGlobal = row*__COL__ + col;
+    
+    return vMaps.at(IdxGlobal);
+}
+
+std::pair<int,int> BeamUnities::RowColIdx2ChipChn(std::pair<int,int> rowcolpair,const std::vector<std::pair<int,int>> vMaps)
+{
+    int rowid = rowcolpair.first; 
+    int colid = rowcolpair.second; 
+
+    return BeamUnities::RowColIdx2ChipChn(rowid,colid,vMaps);
+}
+
+std::pair<int,int> BeamUnities::ChipChn2RowCol(int chipIdx, int chnIdx, const std::vector<std::pair<int,int>> vMaps)
+{
+    if(chipIdx <0 || chipIdx>=__NumChip__ || chnIdx<0 || chnIdx >= __NumChn__)
+    {
+        std::printf(">>>>>>>>>>>>>>>>>>>>>> Error!!! chip chn Idx in ChipChn2RowCol\n");
+        return std::make_pair(-1,-1);
+    }
+    auto chipchnpair = std::make_pair(chipIdx,chnIdx);
+    
+    auto it = std::find(vMaps.cbegin(),vMaps.cend(),chipchnpair);
+
+    auto Idxglobal = std::distance(vMaps.begin(),it);
+
+    return std::make_pair(Idxglobal/__COL__,Idxglobal%__COL__);
+}
+
+std::pair<int,int> BeamUnities::ChipChn2RowCol(std::pair<int,int> chipchnpair,const std::vector<std::pair<int,int>> vMaps)
+{
+    int chipid = chipchnpair.first; 
+    int chnid = chipchnpair.second; 
+    
+    return  BeamUnities::ChipChn2RowCol(chipid,chnid,vMaps);
+}
+
+std::pair<int,int> BeamUnities::Position2RowCol(double px, double py)
+{
+
+    if(px < Xlowboundary || px > X_upboundary || py < Ylowboundary || py > Yupboundary)
+    {
+        return std::make_pair(-1,-1);
+    }
+
+    int rowidx_ = int((px-Xlowboundary)/PixelSpace);
+    int colidx_ = int((py-Ylowboundary)/PixelSpace);
+    
+    auto pixelcenter = BeamUnities::RowColIdx2Position(rowidx_,colidx_);
+    // if (px,py) is in pixel
+    if(fabs(px-pixelcenter.first)<PixelSize/2 && fabs(py-pixelcenter.second)<PixelSize/2.)
+    {
+        return std::make_pair(rowidx_,colidx_) ;
+    }
+    else
+    {
+        return std::make_pair(-1,-1);
+    }
+
+}
+
+std::pair<int,int> BeamUnities::Position2RowCol(std::pair<double,double> positionpair)
+{
+    double px = positionpair.first; 
+    double py = positionpair.second;
+
+    return BeamUnities::Position2RowCol(px,py);
+}
+
+std::pair<double,double> BeamUnities::RowColIdx2Position(int row, int col)
+{
+    if(row >= __ROW__ || col >= __COL__)  
+    {
+        std::printf("=============> Error! row col index\n");
+        std::make_pair(1.,16.);
+    }
+
+    return std::make_pair(row*PixelSpace,col*PixelSpace);
+}
+
+std::pair<double,double> BeamUnities::RowColIdx2Position(std::pair<int,int> rowcolpair)
+{
+    int rowid = rowcolpair.first; 
+    int colid = rowcolpair.second;
+
+    return BeamUnities::RowColIdx2Position(rowid,colid);
+}
