d923514e932bf23711143dfecf57d45c766be328
Lan-sx <shexin@ihep.ac.cn>
2024-09-20 13:11:51 +0800

    Change PixelTPCdata to dataModel 2

---
+++ dataModel/LinkDef.h
@@ -0,0 +1,20 @@
+/*********************************************************************
+ * Author           : Lan-sx
+ * Email            : shexin@ihep.ac.cn
+ * Last modified    : 2024-07-05 21:02
+ * Filename         : LinkDef.h
+ * Description      : LinkDef head file, Data structure of PixelTPC 
+ *                    for ROOT I/O, generate dict
+ * Update           : 2024-07-05 21:02: created
+ * ******************************************************************/
+#include "PixelTPCdata.h"
+#ifdef __CLING__
+
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+
+#pragma link C++ nestedclasses;
+#pragma link C++ class PixelTPCdata+;
+
+#endif
---
+++ dataModel/PixelTPCdata.h
@@ -0,0 +1,78 @@
+/*********************************************************************
+ * Author           : Lan-sx
+ * Email            : shexin@ihep.ac.cn
+ * Last modified    : 2024-07-05 21:05
+ * Filename         : PixelTPCdata.h
+ * Description      : Head file, data structure of PixelTPC Det. R&D 
+ *                    for ROOT I/O, generate dict
+ * Update           : 2024-07-05 21:02: created
+ *                    2024-07-06 18.07: version0_0 3-dim vector type
+ *                    2024-09-19 22.05: change TYPE1 to std::vector<>
+ *                    TODO, vector will used bigger memory, need to update?
+ * ******************************************************************/
+#ifndef __PixelTPCdata_H__
+#define __PixelTPCdata_H__ 1
+//std
+#include <iostream>
+#include <vector>
+#include <array>
+#include <map>
+#include <stdexcept>
+
+//ROOT CERN
+#include "TObject.h"
+
+//User 
+#include "BeamUnities.h"
+
+using PairQT= std::pair<double,double>;            // TYPE3
+using VecSingleChn = std::vector<PairQT>;          // TYPE2 
+//using Array128Chns = std::array<VecSingleChn,128>; // TYPE1 old version
+using Array128Chns = std::vector<VecSingleChn>; // TYPE1
+
+class PixelTPCdata : public TObject
+{
+private:
+    // Triggle_ID (Physics Evt_ID)
+    int fTriggleID;
+    // ======================================================================================================
+    //@Brief: Event Info table 
+    //  std::vector<TYPE1>                 : vector to store all TEPIX chips info
+    //                                       maxima size == Number of TEPIX chips used
+    //  TYPE1 -> std::vector<TYPE2>        : vector to store 128 channels info  
+    //  TYPE2 -> std::vector<TYPE3>        : vector to store (Q,T) info (over threashold) of every channel,
+    //                                       maxima size == 4
+    //  TYPE3 -> std::pair<double,double> :  pair to store (Q,T) info
+    // ======================================================================================================
+    //std::vector<std::array<std::vector<std::pair<double,double>>,128>> fTestdata;
+    std::vector<Array128Chns> fTestdata;
+    
+    //TODO vars need to add 
+    //1. TimeStamp 
+    //2. Flags etc
+    //sub collections
+
+    ClassDef(PixelTPCdata,1);
+
+public:
+    PixelTPCdata(int NumofChips);
+    ~PixelTPCdata() {};
+    
+    PixelTPCdata& SetTiggleID(int triggleid);
+
+    void FillPixelTPCdata(Array128Chns &arr128chns, int chipid);
+    
+    // Getters 
+    int GetTriggleID() { return fTriggleID; }
+    Array128Chns & Getdata_I  (int chipid);
+    VecSingleChn & Getdata_IJ (int chipid, int chnid);
+    //PairQT       & Getdata_IJK(int chipid, int chnid, int kk); 
+    
+    VecSingleChn & operator()(int chipid, int chnid);
+
+    void ClearPixelTPCdata(int NumofChips);
+
+
+};
+
+#endif
