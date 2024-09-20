fb5f6bc0e44f8502855d9e66a3f9c1dc663800f6
Lan-sx <shexin@ihep.ac.cn>
2024-09-20 18:46:28 +0800

    Update LinkDef, for MCTrackdata IO

--- dataModel/LinkDef.h
+++ dataModel/LinkDef.h
@@ -3,11 +3,13 @@
  * Email            : shexin@ihep.ac.cn
  * Last modified    : 2024-07-05 21:02
  * Filename         : LinkDef.h
- * Description      : LinkDef head file, Data structure of PixelTPC 
+ * Description      : LinkDef head file, Data structure  
  *                    for ROOT I/O, generate dict
- * Update           : 2024-07-05 21:02: created
+ * Update           : 2024-07-05 21:02: created and add PixelTPCdata;
+ *                    2024-09-20 15:32: add MCTrackdata;
  * ******************************************************************/
 #include "PixelTPCdata.h"
+#include "MCTrackdata.h"
 #ifdef __CLING__
 
 #pragma link off all globals;
@@ -16,5 +18,6 @@
 
 #pragma link C++ nestedclasses;
 #pragma link C++ class PixelTPCdata+;
+#pragma link C++ class MCTrackdata+;
 
 #endif
